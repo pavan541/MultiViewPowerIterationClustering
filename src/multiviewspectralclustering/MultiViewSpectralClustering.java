50d445d3e07bfeb95002e3b69bcce6e4aa13c591
pavankalyan <pavan@admins-MacBook-Air.local>
2017-01-25 10:46:09 -0500

    Added function for finding euclidean distance

--- src/multiviewspectralclustering/MultiViewSpectralClustering.java
+++ src/multiviewspectralclustering/MultiViewSpectralClustering.java
@@ -9,6 +9,9 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import static java.lang.Math.sqrt;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 /**
  *
@@ -26,10 +29,10 @@ public class MultiViewSpectralClustering {
         int numIter=5;
         
         double[] sigma = new double[3];
-        double sigma1 = sigma[0] = 2.565656085779173;
-        double sigma2 = sigma[1] = 0.888739619532634;
-        double sigma3 = sigma[2] = 2.213400238591733;
-        
+//        double sigma1 = sigma[0] = 2.565656085779173;
+//        double sigma2 = sigma[1] = 0.888739619532634;
+//        double sigma3 = sigma[2] = 2.213400238591733;
+//        
         String x1Path = "/Users/pavan/NetBeansProjects/MultiViewSpectralClustering/src/multiviewspectralclustering/X1.txt";
         String x2Path = "/Users/pavan/NetBeansProjects/MultiViewSpectralClustering/src/multiviewspectralclustering/X2.txt";
         String x3Path = "/Users/pavan/NetBeansProjects/MultiViewSpectralClustering/src/multiviewspectralclustering/X3.txt";
@@ -39,15 +42,21 @@ public class MultiViewSpectralClustering {
         double[][] X2 =  readMatrix(x2Path);
         System.out.println("done 2");
         double[][] X3 =  readMatrix(x3Path);
+               
+//        double[ ] num[ ] = {{1,2}, {2,1}, {3,3}};
+//        double[][] x =EuDist2(num,num);
+        double sigma1 = optSigma(X1);
+        double sigma2 = optSigma(X2);
+        double sigma3 = optSigma(X3);
         
         
-        System.out.println("Single best view ");
-        for(int i=0;i<num_views;i++)
-        {
-            System.out.println("With View "+(i+1));
-            
-        }
-        
+//        System.out.println("Single best view ");
+//        for(int i=0;i<num_views;i++)
+//        {
+//            System.out.println("With View "+(i+1));
+//            
+//        }
+//        
         
     }
     
@@ -78,14 +87,133 @@ public class MultiViewSpectralClustering {
     
     public static double optSigma(double[][] X)
     {
-        int N = 1000;
-        double dist = EuDist2(X,X);
+        int N = X.length;
+        double[][] dist = EuDist2(X,X);
+        double[] newdist = new double[N*N];
+        int i=0;
+        double median = 0;
+        for(int j=0;j<dist[0].length;j++)
+            for(int k=0;k<dist.length;k++)
+            {
+                newdist[i] = dist[j][k];
+                i++;
+            }
+        
+        Arrays.sort(newdist);
+        if(N*N%2 == 0)
+        {
+            int val = (int)N*N/2;
+            median =  (newdist[val-1] + newdist[val])/2;
+        }
+        else
+        {
+            int val = (int)N*N/2;
+            median = newdist[val+1];
+        }
+        System.out.println("median "+median);
         return 0.0;
     }
     
-    public static double EuDist2(double[][] m1, double[][] m2)
+    public static double[][] EuDist2(double[][] m1, double[][] m2)
     {
-        //implementation 
-        return 0.0;
+        int bSqrt =1;
+        
+        int nSmp_a = m1.length,nSmp_b =m2.length;
+        int nFea = m1[0].length;
+        
+        double[][] aa = sumMatrix(m1);
+        double[][] bb = sumMatrix(m2);
+        
+        
+        double[][] m2T = new double[nFea][nSmp_b];
+        
+        for(int i=0;i<nFea;i++)
+            for(int j=0;j<nSmp_b;j++)
+                m2T[i][j] =  m2[j][i];
+        
+        double[][] ab = multiMatrix(m1, m2T);
+
+//        printMatrix(aa);
+//        printMatrix(bb);
+//        printMatrix(ab);
+//        
+        double[][] bbT = new double[bb[0].length][bb.length];
+        
+        for(int i=0;i<bb[0].length;i++)
+            for(int j=0;j<bb.length;j++)
+                bbT[i][j] =  bb[j][i];
+        
+        double[][] aarepmat = new double[nSmp_a][nSmp_b];
+        double[][] bbrepmat = new double[nSmp_a][bbT[0].length];
+        
+        for(int i=0;i<aarepmat.length;i++)
+            for(int j=0;j<aarepmat[0].length;j++)
+            {
+                aarepmat[i][j]=aa[i][0];
+            }
+        
+//        printMatrix(aarepmat);
+        
+        for(int i=0;i<bbrepmat.length;i++)
+            for(int j=0;j<bbrepmat[0].length;j++)
+            {
+                bbrepmat[i][j]=bbT[0][j];
+            }
+        
+      
+        double[][] resMat = new double[nSmp_a][nSmp_b];
+        for(int i=0;i<nSmp_a;i++)
+            for(int j=0;j<nSmp_b;j++) {
+                resMat[i][j] = Math.abs(sqrt(aarepmat[i][j]+bbrepmat[i][j]-2*ab[i][j]));
+            }
+        
+        return resMat;
+    }
+    
+    public static void printMatrix(double[][] m){
+         for(int i=0;i<m.length;i++) {
+            for(int j=0;j<m[0].length;j++)
+                System.out.print(" "+m[i][j]);
+            System.out.println("");
+        }
+    }
+    
+    public static double[][] multiMatrix(double[][] m1,double[][] m2)
+    {
+        int nSmp_a = m1.length,nSmp_b =m2.length;
+        int nFea_a = m1[0].length;
+        int nFea_b = m2[0].length;
+//        System.out.println(" "+nSmp_a+" "+nFea_a+" "+nSmp_b+" "+nFea_b);
+        double[][] ab = new double[nSmp_a][nFea_b];
+        for (int i = 0; i < nSmp_a; i++) {
+            for (int j = 0; j < nFea_b; j++) {
+                ab[i][j] = 0.00000;
+            }
+        }
+
+        for (int i = 0; i < nSmp_a; i++) { // aRow
+            for (int j = 0; j < nFea_b; j++) { // bColumn
+                for (int k = 0; k < nFea_a; k++) { // aColumn
+                    ab[i][j] += m1[i][k] * m2[k][j];
+                }
+            }
+        }
+        return ab;
+    }
+    
+    public static double[][] sumMatrix(double[][] m)
+    {
+        int nSmp = m.length;
+        double[][] sumMat = new double[nSmp][1];
+        for(int i=0;i<nSmp;i++)
+        {
+            double sum =0;
+            for(int j=0;j<2;j++)
+            {
+                sum += m[i][j]*m[i][j];
+            }
+            sumMat[i][0]=sum;
+        }
+        return sumMat;
     }
 }
